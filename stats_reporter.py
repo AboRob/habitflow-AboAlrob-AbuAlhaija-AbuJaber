a652fc8178679b4f0bb93c4e37707e37e05b415b
fawziomar2003-creator <fawziomar2003@gmail.com>
2026-06-12 17:16:27 +0300

    Update stats_reporter.py

--- stats_reporter.py
+++ stats_reporter.py
@@ -2,4 +2,11 @@ import json
 
 
 class StatsReporter:
-    pass
+
+    def print_table(self, stats):
+
+        print("  Habit           Streak   30-Day Rate   Total Logs")
+        print("  --------------- -------- ------------ ------------")
+
+        for s in stats:
+            print(f"  {s['name']:<15} {s['current_streak']:>7}d {s['completion_rate']:>10}% {s['total_logs']:>12}")
